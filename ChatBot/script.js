8946646c340189239afc0517d63fc01f90266936
Arti199424 <115499333+Arti199424@users.noreply.github.com>
2024-01-04 00:35:31 -0800

    Add files via upload

---
+++ ChatBot/script.js
@@ -0,0 +1,95 @@
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatInput = document.querySelector(".chat-input textarea");
+const chatBox = document.querySelector(".chatbox");
+const chatBotToggler = document.querySelector(".chatbot-toggler");
+const chatBotCloseBtn = document.querySelector(".close-btn");
+
+let userMessage;
+const API_KEY="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";
+const inputHeight=chatInput.style.scrollHeight;
+
+const createChatLi = (message, className) => {
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : ` <span class="fa-solid fa-robot"></span>
+  <p></p>`;
+  chatLi.innerHTML=chatContent;
+  chatLi.querySelector("p").textContent=message;
+  return chatLi;
+};
+
+const generateResponse=(incomingChatLi)=>{
+    const API_URL="https://api.openai.com/v1/chat/completions"
+    const messageElement=incomingChatLi.querySelector("p");
+    const requestOption={
+        method:"post",
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+        },
+        body:JSON.stringify({ 
+            "model": "gpt-3.5-turbo",
+    "messages": [
+      {
+        "role": "user",
+        "content": userMessage
+      }
+    ]   
+        })
+    }
+    fetch(API_URL,requestOption)
+    .then((res=>res.json()))
+    .then((data)=>{
+        //console.log(data)
+        messageElement.textContent=data.choices[0].message.content
+    })
+    .catch((error)=>{
+        messageElement.classList.add("error");
+        messageElement.textContent="Oops! Something went wrong.Please try again"
+        //console.log(error);
+    }).finally(()=>{
+        chatBox.scrollTo(0,chatBox.scrollHeight);
+    })
+}
+
+const handleChat = () => {
+  userMessage = chatInput.value;
+  //alert(userMessage);
+  if (!userMessage) return;
+  chatInput.value="";
+ chatBox.append(createChatLi(userMessage, "outgoing")) 
+ chatBox.scrollTo(0,chatBox.scrollHeight)
+ setTimeout(()=>{
+    const incomingChatLi=createChatLi("Thinking...", "incoming")
+    chatBox.append(incomingChatLi);
+    chatBox.scrollTo(0,chatBox.scrollHeight);    
+    generateResponse(incomingChatLi);
+ },600)
+};
+sendChatBtn.onclick = () => {
+  handleChat();
+};
+
+chatBotToggler.onclick=()=>{
+    document.body.classList.toggle('show-chatbot')
+}
+chatBotCloseBtn.onclick=()=>{
+    document.body.classList.remove('show-chatbot')
+}
+
+chatInput.oninput=()=>{
+    chatInput.style.height=`${inputHeight}px`
+    chatInput.style.height=`${chatInput.scrollHeight}px`
+}
+
+chatInput.onkeydown=(e)=>{
+    // console.log(e.key)
+    if(e.key==="Enter" && !e.shiftkey && window.innerWidth>800)
+    {
+        e.preventDefault();//ise lgane se page reload nhi hota terminate ho jata h
+        handleChat();
+    }
+}
